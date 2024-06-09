684d4a5dd50101e3dea8d0c21a244f29300f918b
Om-k <omk.cs20@sahyadri.edu.in>
2024-06-09 17:57:48 +0530

    New Port number for Render

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const userModel = require("./models/user");
 const expenseModel = require("./models/expense");
 const savingModel = require("./models/savings");
 require('dotenv').config(); // Load environment variables
-const port = process.env.PORT || 4000;
+const port = 5000;
 
 console.log('MONGODB_URI:', process.env.MONGODB_URI); // Log the MongoDB URI to verify
 
